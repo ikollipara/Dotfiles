988476578b2bcff60ee5c0acd91bd64f65b25339
Ian Kollipara <ian.kollipara@gmail.com>
2023-03-14 15:29:25 -0500

    feat: Added Neovim Bindings for Moving Windows
    Adjusted bindings to allow for neovim to handle moving between panes.
    This should mean the LSP is not restarting so often, and I can make
    better use of Telescope and such. It also means that tmux will act
    more as a workspace manager, which I am okay with.

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -7,7 +7,18 @@
 --]]
 local mapper = require("modules.mapper")
 
+
+vim.keymap.set({ "n", "i", "v" }, "<M-h>", function() vim.cmd.winc("h") end)
+vim.keymap.set({ "n", "i", "v" }, "<M-j>", function() vim.cmd.winc("j") end)
+vim.keymap.set({ "n", "i", "v" }, "<M-k>", function() vim.cmd.winc("k") end)
+vim.keymap.set({ "n", "i", "v" }, "<M-l>", function() vim.cmd.winc("l") end)
+
+mapper.normal {
+  { lhs = "<leader>S", rhs = vim.cmd.split },
+  { lhs = "<leader>s", rhs = vim.cmd.vsplit },
+}
+
 mapper.visual {
   { lhs = "J", rhs = ":m '>+1<CR>gv=gv" },
-  { lhs = "K", rhs = ":m '<-2<CR>gv=gv" }
+  { lhs = "K", rhs = ":m '<-2<CR>gv=gv" },
 }
