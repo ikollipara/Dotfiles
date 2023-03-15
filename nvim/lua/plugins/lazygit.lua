de6278e6470aabfba96fdefc8440b60734c58258
Ian Kollipara <ian.kollipara@gmail.com>
2023-03-14 22:41:27 -0500

    feat: Added Lazygit for Neovim
    Added Lazygit for git client. I already use it on the terminal, so
    I wanted to include it in Neovim. This means that Neogit was removed.
    I've also updated my aliases to start telescope in the current directory
    for neovim.
    
    breaking: removed Neogit
    breaking: changed 'n' alias to start telescope

---
+++ nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,13 @@
+--[[
+-- lazygit.lua
+-- Ian Kollipara
+-- 2023.03.14
+--
+-- lazygit integration
+--]]
+return {
+  "kdheepak/lazygit.nvim",
+  keys = {
+    { "<leader>gg", "<cmd>LazyGit<cr>" }
+  }
+}
