f86a843ea27518c63f5120e6e506bea6bd3e2fa3
Ian Kollipara <ian.kollipara@gmail.com>
2023-03-27 14:55:22 -0500

    feat(tools): Add TSX support

---
+++ nvim/after/ftplugin/typescriptreact.lua
@@ -0,0 +1,14 @@
+--[[
+-- typescriptreact.lua
+-- Ian Kollipara
+-- 2023.03.27
+--
+-- Typescript React Settings
+--]]
+local opt = vim.opt
+
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.smartindent = true
