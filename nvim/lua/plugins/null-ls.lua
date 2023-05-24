a5ae4d85eb1584e0563bd0d9564e25a7ec6e9e3f
Ian Kollipara <ian.kollipara@gmail.com>
2023-05-24 10:36:53 -0500

    🐛 fix(tools): adjusted null-ls timeout

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -7,43 +7,92 @@
 ---------------------------
 
 return {
-    "jose-elias-alvarez/null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "williamboman/mason.nvim" },
-    opts = function()
-        local nls = require("null-ls")
+	"jose-elias-alvarez/null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = { "williamboman/mason.nvim" },
+	opts = function()
+		local nls = require("null-ls")
 
-        return {
-            root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-            sources = {
-                -- Formatters
-                nls.builtins.formatting.black,
-                nls.builtins.formatting.codespell,
-                nls.builtins.formatting.elm_format,
-                nls.builtins.formatting.eslint,
-                nls.builtins.formatting.isort,
-                nls.builtins.formatting.markdownlint,
-                nls.builtins.formatting.markdown_toc,
-                nls.builtins.formatting.mdformat,
-                nls.builtins.formatting.prettier.with({
-                    extra_filetypes = { "typescriptreact" },
-                    timeout = 10000,
-                }),
-                nls.builtins.formatting.stylua,
+		return {
+			root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+							vim.lsp.buf.format({ bufnr = bufnr })
+						end,
+					})
+				end
+			end,
+			sources = {
+				-- Formatters
+				nls.builtins.formatting.black.with({
+					timeout = 10000,
+				}),
+				nls.builtins.formatting.codespell.with({
+					timeout = 10000,
+				}),
+				nls.builtins.formatting.elm_format.with({
+					timeout = 10000,
+				}),
+				nls.builtins.formatting.eslint.with({
+					extra_filetypes = { "typescriptreact" },
+					timeout = 10000,
+				}),
+				nls.builtins.formatting.isort.with({
+					timeout = 10000,
+				}),
+				nls.builtins.formatting.markdownlint.with({
+					timeout = 10000,
+				}),
+				nls.builtins.formatting.markdown_toc.with({
+					timeout = 10000,
+				}),
+				nls.builtins.formatting.mdformat.with({
+					timeout = 10000,
+				}),
+				nls.builtins.formatting.prettier.with({
+					extra_filetypes = { "typescriptreact" },
+					timeout = 10000,
+				}),
+				nls.builtins.formatting.stylua.with({
+					timeout = 10000,
+				}),
 
-                -- Code Actions
-                nls.builtins.code_actions.eslint,
-                nls.builtins.code_actions.proselint,
+				-- Code Actions
+				nls.builtins.code_actions.eslint.with({
+					extra_filetypes = { "typescriptreact" },
+					timeout = 10000,
+				}),
+				nls.builtins.code_actions.proselint.with({
+					extra_filetypes = { "typescriptreact" },
+					timeout = 10000,
+				}),
 
-                -- Completion
-                nls.builtins.completion.spell,
+				-- Completion
+				nls.builtins.completion.spell.with({
+					timeout = 10000,
+				}),
 
-                -- Diagnostics
-                nls.builtins.diagnostics.flake8,
-                nls.builtins.diagnostics.luacheck,
-                nls.builtins.diagnostics.mypy,
-                nls.builtins.diagnostics.tsc,
-            },
-        }
-    end,
+				-- Diagnostics
+				nls.builtins.diagnostics.flake8.with({
+					timeout = 10000,
+				}),
+				nls.builtins.diagnostics.luacheck.with({
+					timeout = 10000,
+				}),
+				nls.builtins.diagnostics.mypy.with({
+					timeout = 10000,
+				}),
+				nls.builtins.diagnostics.tsc.with({
+					extra_filetypes = { "typescriptreact" },
+					timeout = 10000,
+				}),
+			},
+		}
+	end,
 }
